cafb1ef642526da9e2078b04e4266be44c2bd775
Ggobama <128073688+Ggobama@users.noreply.github.com>
2024-09-20 22:20:49 -0400

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -2,6 +2,15 @@ function getRandomGreeting() {
   var greetings = [
     "Micheal Alexander Lutz Is WEIRD",
     "Fortnite Is The Best Game",
+    "Sigma Sigma On The Wall",
+    "Who's the skibidiest of them all?",
+    "Erm, What The Sigma",
+    "W's in the chat",
+    "Happy Early Birthday!",
+    
+    
+    
+    
     
   ];
   var randomIndex = Math.floor(Math.random() * greetings.length);
