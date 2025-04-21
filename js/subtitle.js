014f7b031eb0cbb24dc24548c02b8e4d4f5b8c35
Ggobama <128073688+Ggobama@users.noreply.github.com>
2025-04-21 12:33:48 -0400

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -1,18 +1,6 @@
 function getRandomGreeting() {
   var greetings = [
-    "Fortnite Is The Best Game",
-    "Sigma Sigma On The Wall",
-    "Who's the skibidiest of them all?",
-    "Erm, What The Sigma",
-    "W's In The chat",
-    "Happy Early Birthday",
-    "1 is fun",
-    "You can't get ahead when you're chased by the dead",
-    "I Love Red 40 Fridays",
-    "Don't forget to play Magma Dash!",
-    "Oh, back so soon I see",
-    "Whats the time? Its OHIO time",
-    "Gen alpha is cooked",
+    ":)",
 
     
     
